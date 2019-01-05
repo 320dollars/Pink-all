0a2ae72e6f80031b3613456bdd1ec7e57ffbde2e
320dollars <44992926+320dollars@users.noreply.github.com>
2019-01-05 09:41:58 +0300

    начал верстку планшетной версии индекса

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,13 @@
 var gulp  = require('gulp'), // Подключаем Gulp
-    sass   = require('gulp-sass'), //Подключаем Sass пакет,
-    browserSync = require('browser-sync'); // Подключаем Browser Sync
+sass   = require('gulp-sass'); //Подключаем Sass пакет,
 
-    gulp.task('mytask', function() {
-    console.log('Привет, я таск!');
-});
-
-var gulp = require('gulp'), // Подключаем Gulp
-    sass  = require('gulp-sass'), //Подключаем Sass пакет,
-    browserSync = require('browser-sync'), // Подключаем Browser Sync
-    concat  = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
-    uglify   = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
-    cssnano  = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
-    rename  = require('gulp-rename'); // Подключаем библиотеку для переименования файлов
+    var gulp = require('gulp'), // Подключаем Gulp
+        sass  = require('gulp-sass'), //Подключаем Sass пакет,
+        browserSync = require('browser-sync'), // Подключаем Browser Sync
+        concat  = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+        uglify   = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+        cssnano  = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+        rename  = require('gulp-rename'); // Подключаем библиотеку для переименования файлов
 
 gulp.task('sass', function(){ // Создаем таск Sass
     return gulp.src('app/sass/**/*.scss') // Берем источник
@@ -32,15 +27,6 @@ gulp.task('default', function () {
         .pipe(gulp.dest('app/css'));
 });
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
-    browserSync({ // Выполняем browserSync
-        server: { // Определяем параметры сервера
-            baseDir: 'app' // Директория для сервера - app
-        },
-        notify: false // Отключаем уведомления
-    });
-});
-
 gulp.task('css-libs', ['sass'], function() {
     return gulp.src('app/css/libs.css') // Выбираем файл для минификации
         .pipe(cssnano()) // Сжимаем
@@ -48,8 +34,6 @@ gulp.task('css-libs', ['sass'], function() {
         .pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
 });
 
-gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+gulp.task('watch', function() {
     gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
-    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-    gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
